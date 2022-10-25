49a6f4aea896a4a7e25a9118e559685d95993ae5
felipeffranco <felipeffranco@outlook.com>
2022-10-25 15:11:27 -0300

    requirements

--- js/game.js
+++ js/game.js
@@ -1,37 +1,53 @@
-let duck1 = document.getElementById('duck1');
-let duck2 = document.getElementById('duck2');
-let duck3 = document.getElementById('duck3');
-let duck4 = document.getElementById('duck4');
-var dog = document.getElementById('dog');
+/*
+**
+** DUCK HUNT JAVASCRIPT
+** Author: Felipe Franco
+**
+*/
 
-function kill1() {
-    duck1.style.display= "none";
-}
+/* 
+NEEDS
+    ducks (maybe an array or object with 2 types of ducks flight/colors)
+    levels (int) 1 - 2 - 3
+    waves - qty of ducks (int) 2 - 3 - 4
+    bullets/life (int) 3 - 4 - 5
+        levei 1 > 2 ducks in 3 waves (6 ducks total) > 3 bullets per wave 
+        level 2 > 3 ducks in 5 waves (15 ducks total) > 4 bullets per wave
+        level 3 > 4 ducks in 7 waves (28 ducks total) > 5 bullets per wave
 
-function kill2() {
-    duck1.style.display= "none";
-}
+SHOW DATA
+    score (int) 100 per duck + 200 per level passed
+    bullets (number)
+    ducks wave/killed (number)
 
-function kill3() {
-    duck1.style.display= "none";
-}
+CONTROLS
+    start (enter)
+    pause (p)
+    mute (m)
+    fullscreen (f)
+    mouse left click (shoot)
 
-function kill4() {
-    duck1.style.display= "none";
-}
+REQUIREMENTS 
+    WIN (> 80% ducks killed per level)
+    LOSE (< 80% ducks killed per level)
+*/
 
-function win() {
-    counter = 0;
-    if(duck1.style.display=="none" && 
-       duck2.style.display=="none" && 
-       duck3.style.display == "none" && 
-       duck4.style.display=="none") {
-        let level = counter++;
-        console.log(`Level ${level}`)
+/*
+After each shoot:
+    if shooted the duck
+        change duck image (shooted) and stay for 2 seconds > IMAGE
+        change duck image (shooted down) > IMAGE
+        show dog image with a duck in it hand > IMAGE
+        paint 1 duck with red color (ducks list) 
+        reduce 1 bullet > TEXT
+        sum 100 in score > TEXT
+    else 
+        blink screen (white)
+        reduce 1 bullet
+*/
+
+class startGame {
+    constructor() {
+        
     }
-    dog.src="./images/dog-win.png";
-}
-
-function lose() {
-    alert(`You Lose!`)    
 }
