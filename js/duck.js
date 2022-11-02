444a064ccd23f08f6fb6178d653a163f9645bb64
felipeffranco <felipeffranco@outlook.com>
2022-11-02 17:00:07 -0300

    doing duck fly

--- js/duck.js
+++ js/duck.js
@@ -4,21 +4,13 @@ export default class Duck {
     }   
 
     randomFlight() {
-        this.xPosition = xPosition; //top
-        this.yPosition = yPosition; //left
-        const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
-        xPosition.style.left= getRandom(0, 1000 - 200)+'px'; // 👈🏼 Horizontally
-        yPosition.style.top = getRandom(0, 300 - 200)+'px'; // 👈🏼 Vertically
+        var top=Math.random();
+        top=top*1000;   
+        var left=Math.random();
+        left=left*1000;                    
         
-/*      const square= document.querySelector('.duck');
-        setInterval(() => {
-            square.style.left= getRandom(0, 300 - 200)+'px'; // 👈🏼 Horizontally
-            square.style.top = getRandom(0, 300 - 200)+'px'; // 👈🏼 Vertically
-        }, 500); // every 1/2 second
- */        
+        const ducks = document.querySelectorAll('.duck');
         
-        document.getElementById("square").style.top=top+"px";                   
-        document.getElementById("square").style.left=left+"px";       
     }
 
     spawnDuck() {
@@ -28,9 +20,7 @@ export default class Duck {
         duck.style.position = 'absolute'
         //setInterval para fazer timer alternando X e Y 
         setInterval(() => {
-            console.log('entrou timer')
-            this.randomFlight; 
-            duck.style.top = this.randomFlight;
+            this.randomFlight();
         }, 500);
         duck.addEventListener('click', this.gotShot)
         document.getElementsByClassName('flight-area')[0].appendChild(duck)
@@ -47,3 +37,5 @@ export default class Duck {
     }
 
 }
+
+
